31ae05d872606e69efd996f8cb7029e7e0ebaeec
wlsdks <wlsdks12@naver.com>
2024-06-23 18:25:40 +0900

    [PULE-6] :recycle: Outbox 서비스 인터페이스 수정

--- src/main/java/com/pulse/event_library/service/OutboxService.java
+++ src/main/java/com/pulse/event_library/service/OutboxService.java
@@ -10,16 +10,10 @@ public interface OutboxService {
     // OutboxEvent를 저장
     void saveOutboxEvent(OutboxEvent event);
 
-    // OutboxEvent의 ID (payload)를 반환
-    Long getOutboxId(OutboxEvent event);
-
     // OutboxEvent를 처리 완료로 표시
     void markOutboxEventProcessed(OutboxEvent event);
 
     // OutboxEvent를 처리 실패로 표시
     void markOutboxEventFailed(OutboxEvent event);
-
-    // OutboxEvent에 해당하는 Kafka 토픽을 반환
-    String getKafkaTopic(OutboxEvent event);
 
 }
