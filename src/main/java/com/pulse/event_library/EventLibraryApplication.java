34482ec346707570928b4bba1e0094d77c8e4c77
wlsdks <wlsdks12@naver.com>
2024-06-23 13:25:32 +0900

    [PULE-2] :sparkles: 프로젝트 추가

---
+++ src/main/java/com/pulse/event_library/EventLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.event_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulse/event_library/EventLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse.event_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
